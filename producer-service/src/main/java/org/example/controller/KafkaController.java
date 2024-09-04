74dbb4f0c7a10114083de11f7d9ba714f48c9bd2
Aleksey Kabanov <phyrocycle.moscow@yandex.ru>
2024-09-04 21:48:01 +0300

    Bitnami Kafka added and project structure fixed

--- producer-service/src/main/java/org/example/controller/KafkaController.java
+++ producer-service/src/main/java/org/example/controller/KafkaController.java
@@ -1,21 +1,27 @@
 package org.example.controller;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.example.kafka.KafkaProducer;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
+@RequestMapping("/kafka/send")
 @RequiredArgsConstructor
 public class KafkaController {
 
     private final KafkaProducer kafkaProducer;
 
-    @PostMapping("/kafka/send")
-    public String send(@RequestBody String message) {
+    @PostMapping
+    public ResponseEntity<String> send(@RequestBody String message) {
+        log.info("Message is: {}", message);
         kafkaProducer.sendMessage(message);
-
-        return "Success";
+        return new ResponseEntity<>("Success", HttpStatus.OK);
     }
 }
